108f7a627737e496114a4b598b07b30afac19246
Amit Chaudhary <amitchaudhary0539@gmail.com>
2022-12-02 17:44:24 +0530

    check static files not found

--- marca_blog/settings.py
+++ marca_blog/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'marca_blog/static'),]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(ROOT_PATH,'static')
 
 
 
